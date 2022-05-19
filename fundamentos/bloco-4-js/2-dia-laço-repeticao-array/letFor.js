344a88ed0a92048c0443f8400edf183ba8c1c0be
Rodrigo Pedrassi Alves <rodpedrassi@gmail.com>
2022-05-19 18:21:52 -0300

    Delete fundamentos/bloco-4-js/2-dia-laço-repeticao-array directory

--- fundamentos/bloco-4-js/2-dia-laço-repeticao-array/letFor.js
+++
@@ -1,7 +0,0 @@
-let numeros = [1, 2, 3, 54, 6, 7, 8, 1];
-
-for (let iterator of numeros) {
-    //isso aqui ta pegando o array e somando +6 em todos os valores o.O
-  iterator += 6;
-  console.log(iterator);
-}
