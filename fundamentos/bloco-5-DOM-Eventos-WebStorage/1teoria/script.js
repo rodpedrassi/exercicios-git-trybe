09a5c74a114d36e7582cfa5c55c6ac8b05905798
rodpedrassi <rodpedrassi@gmail.com>
2022-05-19 15:52:58 -0300

    Parte 1 do course - getElementBy Class ID

---
+++ fundamentos/bloco-5-DOM-Eventos-WebStorage/1teoria/script.js
@@ -0,0 +1,16 @@
+      const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+      const title = document.getElementById("page-title").textContent = "YOLOLO";
+      const p = document.getElementById("paragraph");
+      p.textContent = "YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSsss";
+      p.style.fontSize = "34px";
+      const h4 = document.getElementById("subtitle");
+      h4.textContent = "Que titulao";   
+
+      const cls = document.getElementsByClassName("classao");
+
+      cls[0].style.color = "blue"
+      cls[1].style.fontSize = "54px"
+
+      const titulao = document.getElementsByTagName("h2");
+      titulao[0].style.color = "brown"
